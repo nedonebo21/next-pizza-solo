a8518014832fd4600687a66f0d65df517201f63f
nedonebo21 <cursed.void10007@gmail.com>
2025-11-19 13:25:58 +0400

    feat: add top-bar to home-page

--- src/pages/home/ui/home-page.tsx
+++ src/pages/home/ui/home-page.tsx
@@ -1,13 +1,24 @@
-import { Button } from '@/shared/ui/shadcn/button'
 import { Container } from '@/shared/ui/container'
 import { Typography } from '@/shared/ui/typography'
+import { TopBar } from '@/widgets/top-bar'
 
 export const Home = () => {
   return (
-    <Container className={'mt-10'}>
-      <Typography variant={'title'} as={'h1'} textAlign={'left'}>
-        Все пиццы
-      </Typography>
-    </Container>
+    <>
+      <Container className={'mt-10'}>
+        <Typography variant={'title'} as={'h1'} textAlign={'left'}>
+          Все пиццы
+        </Typography>
+      </Container>
+      <TopBar categories={['Все', 'Мясные', 'Острые', 'Сладкие', 'Вегетарианская', 'С курицей']} />
+      <Container className={'mt-9 pb-14'}>
+        <div className={'flex gap-15'}>
+          <div className={'w-[250px]'}></div>
+          <div className={'flex-1'}>
+            <div className={'flex-col gap-12'}></div>
+          </div>
+        </div>
+      </Container>
+    </>
   )
 }
