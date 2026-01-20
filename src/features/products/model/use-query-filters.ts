07454351b6caf1d875e4faf2f7c2d7a1182b8b6d
nedonebo21 <cursed.void10007@gmail.com>
2026-01-20 16:21:34 +0300

    refactor: hook use-query-filters

--- src/features/products/model/use-query-filters.ts
+++ src/features/products/model/use-query-filters.ts
@@ -1,20 +1,25 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import qs from 'qs'
 import { Filters } from '@/features/products/model/types'
 import { useRouter } from 'next/navigation'
 
 export const useQueryFilters = (filters: Filters) => {
+  const isMounted = useRef(false)
   const router = useRouter()
 
   useEffect(() => {
-    const params = {
-      ...filters.priceRange,
-      pizzaTypes: Array.from(filters.pizzaTypes),
-      sizes: Array.from(filters.sizes),
-      ingredients: Array.from(filters.selectedIngredients),
+    if (isMounted.current) {
+      const params = {
+        ...filters.priceRange,
+        pizzaTypes: Array.from(filters.pizzaTypes),
+        sizes: Array.from(filters.sizes),
+        ingredients: Array.from(filters.selectedIngredients),
+      }
+
+      const query = qs.stringify(params, { arrayFormat: 'comma' })
+      router.push(`?${query}`, { scroll: false })
     }
 
-    const query = qs.stringify(params, { arrayFormat: 'comma' })
-    router.push(`?${query}`, { scroll: false })
+    isMounted.current = true
   }, [filters.priceRange, filters.pizzaTypes, filters.sizes, filters.selectedIngredients, router])
 }
