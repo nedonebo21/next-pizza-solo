954e5576c310ae5a69c7c96d13134fbdc07e5533
nedonebo21 <cursed.void10007@gmail.com>
2026-01-21 15:47:55 +0300

    feat: add product-image

---
+++ src/shared/ui/product-image/product-image.tsx
@@ -0,0 +1,31 @@
+import { cn } from '@/shared/lib/utils'
+
+type ProductImageProps = {
+  className?: string
+  imageUrl: string
+  size: number
+}
+
+export const ProductImage = ({ className, imageUrl, size }: ProductImageProps) => {
+  const classes = cn('relative left-2 top-2 transition-all z-10 duration-300', {
+    'w-[300px] h-[300px]': size === 20,
+    'w-[400px] h-[400px]': size === 30,
+    'w-[500px] h-[500px]': size === 40,
+  })
+
+  return (
+    <div className={cn('flex items-center justify-center flex-1 relative w-full', className)}>
+      <img src={imageUrl} alt={'Logo'} className={classes} />
+      <div
+        className={
+          'absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 border-dashed border-2 rounded-full border-gray-200 w-[450px] h-[450px]'
+        }
+      />
+      <div
+        className={
+          'absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 border-dotted border-2 rounded-full border-gray-100 w-[370px] h-[370px]'
+        }
+      />
+    </div>
+  )
+}
