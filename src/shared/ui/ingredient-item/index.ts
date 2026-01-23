e81aa27e9009b484d8a28f5b1a366408a85eba54
nedonebo21 <cursed.void10007@gmail.com>
2026-01-23 15:33:52 +0300

    feat: add IngredientItem

---
+++ src/shared/ui/ingredient-item/index.ts
@@ -0,0 +1,1 @@
+export { IngredientItem } from './ingredient-item'
---
+++ src/shared/ui/ingredient-item/ingredient-item.tsx
@@ -0,0 +1,41 @@
+import { cn } from '@/shared/lib/utils'
+import { CircleCheck } from 'lucide-react'
+import { Typography } from '@/shared/ui'
+
+type IngredientProps = {
+  className?: string
+  imageUrl: string
+  name: string
+  price: number
+  active?: boolean
+  onClick?: () => void
+}
+
+export const IngredientItem = ({
+  className,
+  name,
+  active,
+  price,
+  onClick,
+  imageUrl,
+}: IngredientProps) => {
+  return (
+    <div
+      className={cn(
+        'flex items-center flex-col p-1 rounded-md w-32 text-center relative cursor-pointer shadow-md bg-white',
+        { 'border border-primary': active },
+        className
+      )}
+      onClick={onClick}
+    >
+      {active && <CircleCheck className={'absolute top-2 right-2 text-primary'} />}
+      <img width={110} height={110} src={imageUrl} alt={'Ingredient'} />
+      <Typography className={'text-xs mb-1'} variant={'bodySemiBold'} as={'span'}>
+        {name}
+      </Typography>
+      <Typography className={'font-bold'} variant={'bodySemiBold'} as={'span'}>
+        {price} ₽
+      </Typography>
+    </div>
+  )
+}
