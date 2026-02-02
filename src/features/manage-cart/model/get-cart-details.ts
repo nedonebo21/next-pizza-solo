9ad35f2e36354d4d397f4e4d89926ea12d643936
nedonebo21 <cursed.void10007@gmail.com>
2026-02-02 11:03:42 +0300

    feat: add get-cart-details

---
+++ src/features/manage-cart/model/get-cart-details.ts
@@ -0,0 +1,29 @@
+import { CartStateItem } from './types'
+import { CartDTO } from '@/shared/services/cart/cart.dto'
+import { calcCartItemPrice } from './calc-cart-item-price'
+
+type ReturnProps = {
+  items: CartStateItem[]
+  totalAmount: number
+}
+
+export const getCartDetails = (data: CartDTO): ReturnProps => {
+  const items = data.items.map(item => ({
+    id: item.id,
+    quantity: item.quantity,
+    name: item.productVariant.product.name,
+    imageUrl: item.productVariant.product.imageUrl,
+    price: calcCartItemPrice(item),
+    pizzaSize: item.productVariant.size,
+    pizzaType: item.productVariant.pizzaType,
+    ingredients: item.ingredients.map(ingredient => ({
+      name: ingredient.name,
+      price: ingredient.price,
+    })),
+  }))
+
+  return {
+    items,
+    totalAmount: data.totalAmount,
+  }
+}
