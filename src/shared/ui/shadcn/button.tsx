9d4db0fb234d5c8026474db5a0989f31e5153c39
nedonebo21 <cursed.void10007@gmail.com>
2026-02-06 16:47:39 +0300

    feat: add isLoading for button

--- src/shared/ui/shadcn/button.tsx
+++ src/shared/ui/shadcn/button.tsx
@@ -5,6 +5,7 @@ import * as React from 'react'
 import { cn } from '@/shared/lib/utils'
 
 import type { VariantProps } from 'class-variance-authority'
+import { Loader2 } from 'lucide-react'
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md active:scale-99 text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
@@ -36,14 +37,17 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean
+  isLoading?: boolean
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, isLoading = false, children, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button'
 
     return (
-      <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
+      <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props}>
+        {!isLoading ? children : <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+      </Comp>
     )
   }
 )
