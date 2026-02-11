b6d2555d8b66efaa516971316c43f3ef0bb5ff72
nedonebo21 <cursed.void10007@gmail.com>
2026-02-12 00:57:07 +0300

    fix: pizzas clone on add to cart (POST request)

--- app/api/cart/route.ts
+++ app/api/cart/route.ts
@@ -55,7 +55,12 @@ export async function POST(req: NextRequest) {
       where: {
         cartId: userCart.id,
         productVariantId: data.productVariantId,
-        ingredients: { every: { id: { in: data.ingredients } } },
+        ingredients: {
+          every: {
+            id: { in: data.ingredients },
+          },
+          some: {},
+        },
       },
     })
 
@@ -68,16 +73,16 @@ export async function POST(req: NextRequest) {
           quantity: findCartItem.quantity + 1,
         },
       })
+    } else {
+      await prisma.cartItem.create({
+        data: {
+          cartId: userCart.id,
+          productVariantId: data.productVariantId,
+          quantity: 1,
+          ingredients: { connect: data.ingredients?.map(id => ({ id })) },
+        },
+      })
     }
-
-    await prisma.cartItem.create({
-      data: {
-        cartId: userCart.id,
-        productVariantId: data.productVariantId,
-        quantity: 1,
-        ingredients: { connect: data.ingredients?.map(id => ({ id })) },
-      },
-    })
 
     const updatedUserCart = await updateCartTotalAmount(token)
 
