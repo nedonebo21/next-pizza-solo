9320761ac71ed7b41eb6da94584efeb12b2f7164
nedonebo21 <cursed.void10007@gmail.com>
2026-02-27 10:28:05 +0300

    feat: added auth by form(email, pass) in nextAuth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,1 @@
-import NextAuth from 'next-auth'
-import GitHubProvider from 'next-auth/providers/github'
-
-export const authOptions = {
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID || '',
-      clientSecret: process.env.GITHUB_SECRET || '',
-    }),
-  ],
-}
-
-const handler = NextAuth(authOptions)
-
-export { handler as GET, handler as POST }
+export { GET, POST } from '@/app/api/next-auth'
