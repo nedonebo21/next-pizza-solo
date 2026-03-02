f679972d1ef62fa935983aec1d3c673089e8bcc4
nedonebo21 <cursed.void10007@gmail.com>
2026-03-02 12:29:56 +0300

    feat: add register-user

---
+++ src/features/auth/api/register-user.ts
@@ -0,0 +1,51 @@
+'use server'
+
+import { Prisma } from '@prisma/client'
+import { prisma } from '../../../../prisma/prisma-client'
+import { hashSync } from 'bcrypt'
+import { sendEmail } from '@/shared/lib'
+import { UserVerification } from '@/shared/ui'
+
+export const registerUser = async (body: Prisma.UserCreateInput) => {
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        email: body.email,
+      },
+    })
+
+    if (user) {
+      if (!user.verified) {
+        throw new Error('Почта не подтверждена')
+      }
+
+      throw new Error('Пользователь уже существует')
+    }
+
+    const createdUser = await prisma.user.create({
+      data: {
+        fullName: body.fullName,
+        email: body.email,
+        password: hashSync(body.password, 10),
+      },
+    })
+
+    const code = Math.floor(100000 + Math.random() * 900000).toString()
+
+    await prisma.verificationCode.create({
+      data: {
+        code,
+        userId: createdUser.id,
+      },
+    })
+
+    await sendEmail(
+      createdUser.email,
+      'Next Pizza | Подтверждение регистрации',
+      UserVerification({ code })
+    )
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
