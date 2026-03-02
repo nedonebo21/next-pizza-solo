73e31426a65c20664a460802729d175e65bd53b8
nedonebo21 <cursed.void10007@gmail.com>
2026-03-02 10:55:11 +0300

    refactor: move auth-schema into entities

--- src/entities/user/index.ts
+++ src/entities/user/index.ts
@@ -1,2 +1,4 @@
 export { GET, POST } from './api/route'
 export { getUserSession } from './model/get-user-session'
+export { registerFormSchema, loginFormSchema } from './model/auth-schema'
+export type { LoginFormValues, RegisterFormValues } from './model/auth-schema'
