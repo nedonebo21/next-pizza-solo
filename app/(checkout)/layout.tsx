5b537b875286daced20af752bd92c1058d7ecb0a
nedonebo21 <cursed.void10007@gmail.com>
2026-03-03 13:00:50 +0300

    fix: add suspense

--- app/(checkout)/layout.tsx
+++ app/(checkout)/layout.tsx
@@ -2,8 +2,7 @@ import { Container } from '@/shared/ui'
 import { Header } from '@/widgets/header'
 
 import type { Metadata } from 'next'
-import type { ReactNode } from 'react'
-
+import { ReactNode, Suspense } from 'react'
 
 export const metadata: Metadata = {
   title: 'Next Pizza | Cart',
@@ -14,7 +13,9 @@ export default function CheckoutLayout({ children }: { children: ReactNode }) {
   return (
     <main className={'min-h-screen bg-[#f4f1ee]'}>
       <Container>
-        <Header hasCart={false} hasSearch={false} className={'border-gray-200'} />
+        <Suspense>
+          <Header hasCart={false} hasSearch={false} className={'border-gray-200'} />
+        </Suspense>
         {children}
       </Container>
     </main>
